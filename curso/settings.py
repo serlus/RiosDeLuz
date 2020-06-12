46150320dc7ab236a607e17a4cec3fcd10b3b2de
serlus <serluscasas@gmail.com>
2020-06-12 15:22:00 -0300

    secret key

--- curso/settings.py
+++ curso/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=osk3e0pb+)1_9z&fidrn=-g)s$&4$2=*$*!va@#j*-a+^n$0)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
