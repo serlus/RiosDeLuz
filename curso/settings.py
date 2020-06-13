f97b9126203d90b4bc2d389bf7bfc69d491a65d6
serlus <serluscasas@gmail.com>
2020-06-13 10:09:39 -0300

    config media e static - close #31

--- curso/settings.py
+++ curso/settings.py
@@ -123,4 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Configuração de ambiente de desenvolvimento
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media'
+MEDIA_URL = os.path.join(BASE_DIR, 'mediafiles')
