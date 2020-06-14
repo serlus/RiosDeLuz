0814e01c74553f3c8c2eb59e56ff14a577164fae
serlus <serluscasas@gmail.com>
2020-06-14 16:49:15 -0300

    hide group

--- curso/base/admin.py
+++ curso/base/admin.py
@@ -8,7 +8,7 @@ from django.contrib.auth.forms import (
     UserChangeForm,
     UserCreationForm,
 )
-from django.contrib.auth.models import Group
+# from django.contrib.auth.models import Group
 from django.core.exceptions import PermissionDenied
 from django.db import router, transaction
 from django.http import Http404, HttpResponseRedirect
