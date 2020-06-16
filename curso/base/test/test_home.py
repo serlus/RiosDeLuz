353190c660e4ff8cdcd0dec2cd7481ec55e21a01
serlus <serluscasas@gmail.com>
2020-06-16 11:16:48 -0300

    add test content

--- curso/base/test/test_home.py
+++ curso/base/test/test_home.py
@@ -1,6 +1,27 @@
+from curso.django_assertions import assert_contains
 from django.test import Client
 
 
 def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
+
+def test_status_code(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<title>Rios de Luz</title>')
+
+def test_status_code(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<meta name="description" content="Source code generated using layoutit.com">')
+
+def test_status_code(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<meta name="author" content="LayoutIt!">')
+
+def test_status_code(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<a class="navbar-brand" href="#">Navbar</a>')
+
+def test_status_code(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<a class="navbar-brand" href="#">Navbar</a>')
