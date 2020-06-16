11d131ad7deb7da8c17f33fabf1c6b4dbf6dc1e5
serlus <serluscasas@gmail.com>
2020-06-16 11:26:54 -0300

    flake8

--- curso/base/test/test_home.py
+++ curso/base/test/test_home.py
@@ -6,21 +6,26 @@ def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
 
+
 def test_title(client: Client):
     resp = client.get('/')
     assert_contains(resp, '<title>Rios de Luz</title>')
+
 
 def test_description(client: Client):
     resp = client.get('/')
     assert_contains(resp, '<meta name="description" content="Source code generated using layoutit.com">')
 
+
 def test_author(client: Client):
     resp = client.get('/')
     assert_contains(resp, '<meta name="author" content="LayoutIt!">')
 
+
 def test_navbar_brand(client: Client):
     resp = client.get('/')
     assert_contains(resp, '<a class="navbar-brand" href="#">Navbar</a>')
+
 
 def test_nav_link_home(client: Client):
     resp = client.get('/')
