c2b9806876653c127ce7dc6d1c321dd0cad190c4
serlus <serluscasas@gmail.com>
2020-06-17 11:40:27 -0300

    pag seguro - close #87

--- curso/base/test/test_home.py
+++ curso/base/test/test_home.py
@@ -17,13 +17,5 @@ def test_title(resp):
     assert_contains(resp, '<title>Rios de Luz</title>')
 
 
-def test_description(resp):
-    assert_contains(resp, '<meta name="description" content="página do curso Rios de Luz">')
-
-
-def test_author(resp):
-    assert_contains(resp, '<meta name="author" content="Sergio Casas">')
-
-
 def test_home_link(resp):
     assert_contains(resp, f'href="{reverse("base:home")}">Rios de Luz</a>')
