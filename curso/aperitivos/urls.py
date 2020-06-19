60be80c3af2126ae35ddbe8deb7c268eb8f6327f
serlus <serluscasas@gmail.com>
2020-06-19 18:47:06 -0300

    elemento for  - close #106

--- curso/aperitivos/urls.py
+++ curso/aperitivos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from curso.aperitivos.views import video
+from curso.aperitivos.views import video, indice
 
 
 app_name = 'aperitivos'
 urlpatterns = [
     path('<slug:slug>', video, name='video'),
+    path('', indice, name='indice'),
 ]
--- curso/aperitivos/views.py
+++ curso/aperitivos/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+videos = [
+        {'slug':'motivacao', 'titulo': 'Recados: Motivação', 'vimeo_id': 430007361},
+        {'slug':'atendimentos', 'titulo': 'Recados: Atendimentos', 'vimeo_id': 430004576},
+    ]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Recados: Motivação', 'vimeo_id': 430007361},
-        'atendimentos': {'titulo': 'Recados: Atendimentos', 'vimeo_id': 430004576},
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
