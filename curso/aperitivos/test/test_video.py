ac4ede8f393bc14e7f6da17a34ad3236a355cea0
serlus <serluscasas@gmail.com>
2020-06-22 08:15:48 -0300

    falha nos test video

--- curso/aperitivos/test/test_video.py
+++ curso/aperitivos/test/test_video.py
@@ -1,21 +1,36 @@
 import pytest
 
+from django import db
 from django.urls import reverse
 from curso.django_assertions import assert_contains
+from curso.aperitivos.models import Video
+
+@pytest.fixture
+def video(db):
+    v = Video(slug='motivacao', titulo='Recados: Motivação', vimeo_id='430007361')
+    v.save()
+    return v
+
+@pytest.fixture
+def resp(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug,)))
 
 
 @pytest.fixture
-def resp(client, db):
-    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+def resp_video_nao_encontrado(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug + 'video_nao_existente',)))
+
+def test_status_code_video_nao_encotrado(resp_video_nao_encontrado):
+    assert resp_video_nao_encontrado.status_code == 404
 
 
 def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_title_video(resp):
-    assert_contains(resp, 'Recados: Motivação')
+def test_title_video(resp, video):
+    assert_contains(resp, video.titulo)
 
 
-def test_conteudo_video(resp):
-    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/430007361"')
+def test_conteudo_video(resp, video):
+    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/{video.vimeo_id}"')
