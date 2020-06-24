f4690683c83c46ef84ad88d0a15326fe7fdc136b
serlus <serluscasas@gmail.com>
2020-06-24 19:36:42 -0300

    introdução ao admin - close #134

---
+++ curso/aperitivos/admin.py
@@ -0,0 +1,9 @@
+from django.contrib.admin import ModelAdmin, register
+from curso. aperitivos.models import Video
+
+
+@register(Video)
+class VideoAdmin(ModelAdmin):
+    list_display = ('titulo', 'slug', 'creation', 'vimeo_id')
+    ordering = ('creation',)
+    prepopulated_fields = {'slug':('titulo',)}
