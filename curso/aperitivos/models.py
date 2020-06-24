4bc82b5e8bb82f4abc0c289eac5beb420e7d47a2
serlus <serluscasas@gmail.com>
2020-06-24 17:48:36 -0300

    add migrate n test

--- curso/aperitivos/models.py
+++ curso/aperitivos/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 
 
 class Video(models.Model):
     slug = models.CharField(max_length=32)
     titulo = models.CharField(max_length=32)
     vimeo_id = models.CharField(max_length=32)
+    creation = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
         return reverse('aperitivos:video', args=(self.slug,))
