36f09f4a303585e20e90c010d3d4c31fbffc5f39
serlus <serluscasas@gmail.com>
2020-06-25 19:24:51 -0300

    add ordered

--- curso/settings.py
+++ curso/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'curso.base',
     'curso.aperitivos',
-    'curso.modulos'
+    'curso.modulos',
+    'ordered_model',
 ]
 
 MIDDLEWARE = [
