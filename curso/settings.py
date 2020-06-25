4fb04c2c4b109291f1498928fb59d1efbea59465
serlus <serluscasas@gmail.com>
2020-06-25 11:28:26 -0300

    add context processors

--- curso/settings.py
+++ curso/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'curso.modulos.context_processors.listar_modulos',
             ],
         },
     },
