423d65107acbe9ba1d4a4443c68cd180647ff9e7
serlus <serluscasas@gmail.com>
2020-06-26 13:45:08 -0300

    criando relacionamentos  modulos aulas

--- curso/modulos/admin.py
+++ curso/modulos/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from ordered_model.admin import OrderedModelAdmin
 
-from curso.modulos.models import Modulo
+from curso.modulos.models import Aula, Modulo
 
 
 # Register your models here.
 @admin.register(Modulo)
 class ModuloAdmin(OrderedModelAdmin):
     list_display = ('titulo', 'publico', 'move_up_down_links')
+    prepopulated_fields = {'slug': ('titulo',)}
+
+
+@admin.register(Aula)
+class AulaAdmin(OrderedModelAdmin):
+    list_display = ('titulo', 'modulo', 'move_up_down_links')
+    list_filter = ('modulo',)
     prepopulated_fields = {'slug': ('titulo',)}
