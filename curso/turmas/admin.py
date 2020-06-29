89141bca3f23415898b48c2063c1710c2d0a7a74
serlus <serluscasas@gmail.com>
2020-06-29 08:19:45 -0300

    admin matricula - close #184

--- curso/turmas/admin.py
+++ curso/turmas/admin.py
@@ -3,8 +3,16 @@ from django.contrib import admin
 from curso.turmas.models import Turma
 
 
+class MatriculaInLine(admin.TabularInline):
+    model = Turma.alunos.through
+    extra = 1
+    readonly_fields = ('data',)
+    autocomplete_fields = ('usuario',)
+    ordering = ('data',)
+
 @admin.register(Turma)
 class TurmaAdmin(admin.ModelAdmin):
+    inlines = [MatriculaInLine]
     list_display = ('nome', 'slug', 'inicio', 'fim')
     prepopulated_fields = {'slug': ('nome',)}
     ordering = ('-inicio',)
