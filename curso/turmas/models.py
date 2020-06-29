2a0d67fe745d0549893dd29874b45905620dd76e
serlus <serluscasas@gmail.com>
2020-06-28 22:09:46 -0300

    Revert "Merge pull request #203 from serlus/180"
    This reverts commit a6f12612003b160dfdfafad6ae0d991e7134f1a2, reversing
    changes made to 7c0fbc014f1a565e46eb97f030221d576e02cadc.

--- curso/turmas/models.py
+++ curso/turmas/models.py
@@ -1,17 +1,9 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
 
 # Create your models here.
 class Turma(models.Model):
-    """
-    classe turma,
-    matriculas esta usando get_user_model para herdar o UserModel
-    independente de onde esteja, com base no relacionamento muitos
-    para muitos (ManyToManyField).
-    """
     nome = models.CharField(max_length=64)
     slug = models.SlugField(max_length=64)
     inicio = models.DateField()
     fim = models.DateField()
-    matriculas = models.ManyToManyField(get_user_model())
